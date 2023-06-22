037427c62c6ebf7557e961b4d761d5124c08de17
AndyPan <andyafter@gmail.com>
2023-06-22 18:25:24 +0800

    Added readme and stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # curve-marking
+
+Curve Marking Technique for Sante Energy
+
+## Product Symbols
+
+| Product            | Symbol |
+| ------------------ | ------ |
+| Brent              | I      |
+| Barge Crack        | BOA    |
+| Fuel Oil East/West | SJS    |
+| Visco              | STS    |
+| 180                | SZS    |
--- main.py
+++ main.py
@@ -23,8 +23,11 @@ def mark(product, mark_month, new_marked_value):
     # Create a complete LaTeX document
     latex_document = r"""
     \documentclass{article}
+    \usepackage{helvet}  % Load the Helvetica font package
     \begin{document}
+    {\fontfamily{phv}\selectfont  % Start a group with the Helvetica font
     """ + latex_table + r"""
+    }  % End the group
     \end{document}
     """
 
@@ -35,6 +38,31 @@ def mark(product, mark_month, new_marked_value):
     # Use pdflatex to generate a PDF from the .tex file
     os.system('pdflatex table.tex')
 
+
+def mark_380(brent, barge, sjs):
+    with open(brent, 'r') as f:
+        reader = csv.reader(f)
+        brent_data = list(reader)
+    with open(barge, 'r') as f:
+        reader = csv.reader(f)
+        barge_data = list(reader)
+    with open(sjs, 'r') as f:
+        reader = csv.reader(f)
+        sjs_data = list(reader)
+
+    data = []
+    for i in range(min(len(brent_data), len(barge_data), len(sjs))):
+        t = brent_data[i][0]
+        value = (float(brent_data[i][1])+ float(barge_data[i][1]))*6.35 + float(sjs_data[i][1])
+        data.append([t, round(value,3)])
+    return data
+
+
+def mark_gasoline():
+
+    pass
+
 if __name__ == '__main__':
-    product = "0.5.csv"
-    mark(product, "Aug23/Sep23", "8.5")
+    product = "data/0.5.csv"
+    # mark(product, "Aug23/Sep23", "8.5")
+    print(mark_380("data/brent.csv", "data/barge.csv", "data/sjs.csv"))
